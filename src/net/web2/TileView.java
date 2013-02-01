86a0472f21b1d4b18090c588a703af63d5111d02
Vincent Dubois <dubois.marseille@wanadoo.fr>
2013-02-01 13:17:47 +0100

    Débuggages divers...

--- src/net/web2/TileView.java
+++ src/net/web2/TileView.java
@@ -22,6 +22,7 @@ import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Matrix;
 import android.graphics.Paint;
+import android.graphics.RectF;
 import android.graphics.drawable.Drawable;
 import android.util.AttributeSet;
 import android.util.FloatMath;
@@ -42,7 +43,7 @@ public class TileView extends View {
      * dimensions. X/Y Tile Counts are the number of tiles that will be drawn.
      */
 
-    protected static int mTileSize = 50;
+    protected static int mTileSize = 40;
     int w; int h;
 
     protected static int mXTileCount;
@@ -98,8 +99,6 @@ public class TileView extends View {
     private final Paint mPaint = new Paint();
     
     void init(){
-        mXTileCount=15;
-        mYTileCount=10;
 
         initTileView();
         mTileGrid = new int[][]{
@@ -112,8 +111,10 @@ public class TileView extends View {
         		{0,1,2,0,0,2,1,0,0,1,1,1,1,0,0},
         		{0,1,1,1,1,1,1,0,0,0,0,2,1,0,0},
         		{0,0,0,0,0,0,0,0,0,0,0,0,1,2,0},
-        		{0,0,0,0,0,0,0,0,0,0,0,0,1,1,1},
+        		{0,0,0,0,0,0,0,0,0,0,0,0,1,1,1}
         		};
+        mXTileCount=mTileGrid.length;
+        mYTileCount=mTileGrid[0].length;
     }
 
     public void ajout(int x, int y){
@@ -130,41 +131,17 @@ public class TileView extends View {
     
     public TileView(Context context, AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
-        
-        
-        
-        };
-        
-/*        TypedArray a = context.obtainStyledAttributes(attrs, R.styleable.TileView);
-
-        mTileSize = a.getInt(R.styleable.TileView_tileSize, 12);
-        
-        a.recycle();*/
-    
+        init();
+        };    
 
     public TileView(Context context, AttributeSet attrs) {
         super(context, attrs);
-        
         init();
-        
-/*
-        TypedArray a = context.obtainStyledAttributes(attrs, R.styleable.TileView);
-
-        mTileSize = a.getInt(R.styleable.TileView_tileSize, 12);
-        
-        a.recycle();*/
     }
 
     public TileView(Context context) {
         super(context);
-        
-        
-/*
-        TypedArray a = context.obtainStyledAttributes(attrs, R.styleable.TileView);
-
-        mTileSize = a.getInt(R.styleable.TileView_tileSize, 12);
-        
-        a.recycle();*/
+        init();
     }
 
     
@@ -180,19 +157,7 @@ public class TileView extends View {
     	mTileArray = new Bitmap[tilecount];
     }
 
-/*
-    @Override
-    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
-        mXTileCount = (int) Math.floor(w / mTileSize);
-        mYTileCount = (int) Math.floor(h / mTileSize);
 
-        mXOffset = ((w - (mTileSize * mXTileCount)) / 2);
-        mYOffset = ((h - (mTileSize * mYTileCount)) / 2);
-
-        mTileGrid = new int[mXTileCount][mYTileCount];
-        clearTiles();
-    }
-*/
     /**
      * Function to set the specified Drawable as the tile for a particular
      * integer key.
@@ -213,13 +178,13 @@ public class TileView extends View {
      * Resets all tiles to 0 (empty)
      * 
      */
-    public void clearTiles() {
+/*    public void clearTiles() {
         for (int x = 0; x < mXTileCount; x++) {
             for (int y = 0; y < mYTileCount; y++) {
                 setTile(0, x, y);
             }
         }
-    }
+    }*/
 
     /**
      * Used to indicate that a particular tile (set with loadTile and referenced
@@ -284,7 +249,7 @@ public class TileView extends View {
     @Override
     protected void onSizeChanged(int largeur, int hauteur, int ancien_largeur, int ancien_hauteur) {
     	super.onSizeChanged(largeur, hauteur, ancien_largeur, ancien_hauteur);
-/*
+
 
         mXOffset = ((largeur - (mTileSize * mXTileCount)) / 2);
         mYOffset = ((hauteur - (mTileSize * mYTileCount)) / 2);
@@ -295,7 +260,7 @@ public class TileView extends View {
 		RectF rectVoulu = new RectF(0, 0, largeur, hauteur);
 		RectF rectReel = new RectF(0, 0, ancien_largeur, ancien_hauteur);
 		transform.setRectToRect(rectVoulu, rectReel, Matrix.ScaleToFit.CENTER);	
-		transform.invert(intransform);*/
+		transform.invert(intransform);
 	}
     
 }
