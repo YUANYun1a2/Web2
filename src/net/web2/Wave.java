c66afb2ed4a3f49adc17411e7c46650d74f30f81
Stelarius <cadianre@hotmail.com>
2013-03-13 23:20:27 +0100

    Suppression du monstre lorsqu'il arrive au bout

--- src/net/web2/Wave.java
+++ src/net/web2/Wave.java
@@ -9,7 +9,7 @@ import android.graphics.Canvas;
 public class Wave {
 	Bitmap bitmap;
 	ArrayList<Monstre> liste_monstres;
-	
+
 	public Wave(Bitmap bitmap, Chemin chemin) {
 		this.bitmap = bitmap;
 		liste_monstres = new ArrayList<Monstre>();
@@ -17,18 +17,29 @@ public class Wave {
 		liste_monstres.add(new Monstre(bitmap, 1, this,chemin));
 		liste_monstres.add(new Monstre(bitmap, 2, this,chemin));
 	}
-	
+
 	public void draw(Canvas canvas) {
 		for(Monstre monstre: liste_monstres){
 			monstre.draw(canvas);
 			canvas.drawBitmap(monstre.bitmap, monstre.getX(), monstre.getY(), monstre.paint);
 		}
 	}
-	
+
 	public void move(){
 		for(Monstre monstre: liste_monstres){
 			monstre.move();
 		}
 	}
-
+	
+	public boolean arrived(Chemin chemin){
+		Iterator<Monstre> it = this.liste_monstres.iterator();
+		while(it.hasNext()){
+			Monstre monstre = it.next();
+			if(monstre.position == chemin.getFinal){
+				it.remove();
+				return true;
+			}
+		}
+		return false;
+	}
 }
